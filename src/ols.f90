75fcbb466a9b4859865ca02b8eee9f00bfffed61
Anders Johansson <anjohan@uio.no>
2018-10-31 14:01:58 +0100

    Switched to DEGLSD, made it work with p > N

--- src/ols.f90
+++ src/ols.f90
@@ -31,21 +31,28 @@ module mod_ols
             real(dp), intent(in) :: y_values(:)
             real(dp), intent(in), optional :: x_values(:,:)
 
-            integer :: lwork, info, N, p, rank
+            integer :: lwork, info, N, p, rank, liwork
             real(dp) :: rcond
             real(dp), allocatable :: work(:), X(:,:), y(:), s(:)
+            integer, allocatable :: iwork(:)
 
             if (present(x_values)) call self%create_X(x_values)
 
             lwork = -1
-            allocate(work(1))
+            allocate(work(1), iwork(1))
 
             N = size(y_values)
             p = size(self%basis)
 
             ! copies, dgels modifies
             X = self%X
-            y = y_values
+
+            if (N > p) then
+                y = y_values
+            else
+                allocate(y(p))
+                y(1:N) = y_values
+            end if
 
 !            call dgels("N", N, p, 1, X, N, y, N, work, lwork, info)
 !            lwork = nint(work(1))
@@ -58,13 +65,18 @@ module mod_ols
             allocate(s(p))
             rcond = -1.0d0
 
-            call dgelss(N, p, 1, X, N, y, N, s, rcond, rank, work, lwork, info)
+!            call dgelss(N, p, 1, X, N, y, N, s, rcond, rank, work, lwork, info)
+            call dgelsd(N, p, 1, X, N, y, max(N,p), s, rcond, rank, work, lwork, iwork, info)
             lwork = nint(work(1))
             deallocate(work)
             allocate(work(lwork))
-            call dgelss(N, p, 1, X, N, y, N, s, rcond, rank, work, lwork, info)
+            liwork = iwork(1)
+            deallocate(iwork)
+            allocate(iwork(liwork))
+            call dgelsd(N, p, 1, X, N, y, max(N,p), s, rcond, rank, work, lwork, iwork, info)
+!            call dgelss(N, p, 1, X, N, y, N, s, rcond, rank, work, lwork, info)
 
-            call check_info(info, "DGELSS")
+            call check_info(info, "DGELSD")
 
             self%beta = y(1:p)
         end subroutine
